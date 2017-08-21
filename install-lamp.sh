a8796f84fb52798b1a73b28a7cc7110d06afb103
Usman Ali Qureshi <usman@usmanaliqureshi.com>
2017-08-21 19:13:01 +0500

    Update install-lamp.sh

--- install-lamp.sh
+++ install-lamp.sh
@@ -2,8 +2,8 @@
 #
 # Author: Usman Ali Qureshi (usman@usmanaliqureshi.com)
 #
-# More Scripts available at https://github.com/usmanaliqureshi/linux
-#
+# More Scripts are available at https://github.com/usmanaliqureshi/linux
+
 ### Checking if the script is running from root ###
 if [[ `id -u` -ne 0 ]] ;
 then
@@ -12,21 +12,25 @@ echo "   Please run as root by typing sudo ./install-lamp.sh   "
 echo "" ;
 exit 1 ;
 fi
+
 echo ""
 echo "### Installing LAMP Server ###"
 echo ""
 sudo apt-get update
 echo ""
+
 echo "### Installing Apache2 ###"
 echo ""
 sudo apt-get install apache2
 echo ""
+
 echo "### Installing MySQL-Server & Php5-MySQL ###"
 echo ""
 sudo apt-get install mysql-server php5-mysql
 sudo mysql_install_db
 sudo mysql_secure_installation
 echo ""
+
 echo "### Installing Php5 and relevant packages ###"
 echo ""
 sudo apt-get install php5 libapache2-mod-php5 php5-mcrypt
@@ -36,16 +40,19 @@ echo "<IfModule mod_dir.c>" >> /etc/apache2/mods-enabled/dir.conf
 echo "    DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm" >> /etc/apache2/mods-enabled/dir.conf
 echo "</IfModule>" >> /etc/apache2/mods-enabled/dir.conf
 echo ""
+
 echo "### Restarting Apache2 ###"
 echo ""
 sudo service apache2 restart
 echo ""
+
 echo "### Installing Php5-CLI ###"
 echo ""
 sudo apt-get install php5-cli
 touch /var/www/html/info.php
 echo "<?php phpinfo(); ?>"
 echo ""
+
 echo "### Php Version Info ###"
 sudo php -v
 echo ""
