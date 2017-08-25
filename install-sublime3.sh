f08b4206b5748f56017f51c2ef678a8d7be991ea
Usman Ali Qureshi <usman@usmanaliqureshi.com>
2017-08-25 20:19:34 +0500

    Update install-sublime3.sh

--- install-sublime3.sh
+++ install-sublime3.sh
@@ -12,6 +12,12 @@ echo "   Please run as root by typing sudo ./install-sublime3.sh   "
 echo "" ;
 exit 1 ;
 fi
+
+echo ""
+echo "  -> Installing Sublime Text 3  "
+echo ""
 sudo add-apt-repository ppa:webupd8team/sublime-text-3
 sudo apt-get update
 sudo apt-get install sublime-text-installer
+echo "Sublime Text 3 is installed successfully"
+echo ""
