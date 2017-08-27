40d3fcc3b9ddb0980c81e4ac380b7891f84a04b6
Usman Ali Qureshi <usman@usmanaliqureshi.com>
2017-08-27 19:56:59 +0500

    Delete: Duplicate script

--- installwine.sh
+++
@@ -1,7 +0,0 @@
-sudo add-apt-repository ppa:ubuntu-wine/ppa
-sudo apt-get update
-sudo apt-get install wine1.8 winetricks
-sudo dpkg --add-architecture i386
-sudo add-apt-repository ppa:wine/wine-builds
-sudo apt-get update
-sudo apt-get install --install-recommends winehq-devel
