c56c3904c527d424f19fb64555a71f5a24cb3461
Usman Ali Qureshi <usman@usmanaliqureshi.com>
2017-08-27 19:59:46 +0500

    Update install-wine.sh

--- install-wine.sh
+++ install-wine.sh
@@ -1,7 +1,31 @@
-sudo add-apt-repository ppa:ubuntu-wine/ppa
-sudo apt-get update
-sudo apt-get install wine1.8 winetricks
-sudo dpkg --add-architecture i386
-sudo add-apt-repository ppa:wine/wine-builds
-sudo apt-get update
-sudo apt-get install --install-recommends winehq-devel
+#!/bin/bash
+#
+# Author: Usman Ali Qureshi (usman@usmanaliqureshi.com)
+#
+# More Scripts available at https://github.com/usmanaliqureshi/linux
+#
+
+### Checking if the script is running from root ###
+if [[ `id -u` -ne 0 ]] ;
+then
+echo ""
+echo "   Please run as root by typing sudo ./install-wine.sh   "
+echo "" ;
+exit 1 ;
+fi
+
+echo ""
+echo "#####################################"
+echo "#                                   #"
+echo "#      Installing Wine 1.8          #"
+echo "#                                   #"
+echo "#####################################"
+echo ""
+
+add-apt-repository ppa:ubuntu-wine/ppa
+apt-get update
+apt-get install wine1.8 winetricks
+dpkg --add-architecture i386
+add-apt-repository ppa:wine/wine-builds
+apt-get update
+apt-get install --install-recommends winehq-devel
